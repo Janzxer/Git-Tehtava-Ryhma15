4d2ddabc17367f4f5e570d0ef7e3c94b583608ad
Janne <janne.viljanen@hotmail.com>
2024-11-10 19:41:15 +0200

    Added a gnome

--- src/App.java
+++ src/App.java
@@ -2,5 +2,23 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Alku testaus että toimiiko.");
         // tuleeko tämä läpi /koomates
+
+        gnome();
+    }
+
+    public static void gnome()
+    {
+        System.out.println("                __");
+        System.out.println("             .-'  |");
+        System.out.println("            /   <\\|");
+        System.out.println("           /     \\'");
+        System.out.println("           |_.- o-o");
+        System.out.println("           / C  -._)\\");
+        System.out.println("          /',        |");
+        System.out.println("         |   `-,_,__,'");
+        System.out.println("         (,,)====[_]=|");
+        System.out.println("           '.   ____/");
+        System.out.println("            | -|-|_");
+        System.out.println("            |____)_)");
     }
 }
